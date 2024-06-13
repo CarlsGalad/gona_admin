f34a78c9df2160546c7ce6068290fc2bc8bfeffe
Adams Kingsley <Yelsgink@gmail.com>
2024-06-13 09:59:23 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,18 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gona_admin/account_creation/auth_page.dart';
-
 import 'package:gona_admin/firebase_options.dart';
-
-import 'watcher.dart';
-
-// import 'home/admin_home.dart';
+import 'package:gona_admin/watcher.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase with specified options
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.web,
   );
 
-  // Create an instance of the OrderWatcherService
-  OrderWatcherService orderWatcherService = OrderWatcherService();
-
-  // Start watching order items
-  orderWatcherService.startWatching();
-
+  // Start the app after initializing services
   runApp(const GonaAdmin());
 }
 
@@ -27,9 +21,30 @@ class GonaAdmin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: AuthPage(),
-      debugShowCheckedModeBanner: false,
+    // Initialize and start OrderWatcherService after the app is built
+    return FutureBuilder(
+      future: _initializeServices(),
+      builder: (context, snapshot) {
+        // Show a loading spinner until the services are initialized
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(
+              child: Text('Error initializing services: ${snapshot.error}'));
+        } else {
+          return const MaterialApp(
+            home: AuthPage(),
+            debugShowCheckedModeBanner: false,
+          );
+        }
+      },
     );
+  }
+
+  // Function to initialize services asynchronously
+  Future<void> _initializeServices() async {
+    // Create an instance of the OrderWatcherService and start watching
+    OrderWatcherService orderWatcherService = OrderWatcherService();
+    orderWatcherService.startWatching();
   }
 }
