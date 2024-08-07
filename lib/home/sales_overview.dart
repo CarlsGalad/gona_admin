f177d6a97bfbc275e566c276b5c918babadcea8e
Adams Kingsley <Yelsgink@gmail.com>
2024-08-07 23:31:47 +0100

    Update sales_overview.dart
    UI UX Modification

--- lib/home/sales_overview.dart
+++ lib/home/sales_overview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SalesOverviewTile extends StatefulWidget {
   const SalesOverviewTile({super.key});
@@ -12,106 +13,106 @@ class _SalesOverviewTileState extends State<SalesOverviewTile> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-        width: 500,
-        height: 250,
-        decoration: BoxDecoration(
-          gradient: const LinearGradient(
-            begin: Alignment.bottomLeft,
-            colors: [
-              Colors.blueGrey,
-              Colors.deepPurple,
-              Colors.deepPurpleAccent,
-              Colors.teal,
-              Colors.tealAccent,
-              Colors.blueGrey,
-            ],
-          ),
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 15),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  'Sales Overview',
-                  style: TextStyle(fontWeight: FontWeight.bold),
+      child: Card(
+        elevation: 1,
+        color: Colors.white70,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: SizedBox(
+          width: 500,
+          height: 250,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'Manage',
+                    style: GoogleFonts.abel(fontWeight: FontWeight.bold),
+                  ),
                 ),
-              ),
-              Row(
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                        color: Colors.black38,
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                    child: const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text(
-                          'View',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, color: Colors.grey),
-                        )),
-                  ),
-                  const Spacer(),
-                  const Icon(
-                    Icons.language,
-                    color: Colors.grey,
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.all(15.0),
-                    child: Text('Top Vendors'),
-                  ),
-                ],
-              ),
-              const Row(
-                children: [
-                  Text(
-                    'Manage Vendors',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              Row(
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                        color: Colors.black38,
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                    child: const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text(
-                          'View',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, color: Colors.grey),
-                        )),
-                  ),
-                  const Spacer(),
-                  const Icon(
-                    Icons.people,
-                    color: Colors.grey,
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.all(15.0),
-                    child: Text('Top Customers'),
-                  ),
-                ],
-              ),
-              const Row(
-                children: [
-                  Text(
-                    'Manage Customers',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                ],
-              ),
-            ],
+                Row(
+                  children: [
+                    MaterialButton(
+                      height: 20,
+                      color: Colors.orange.shade200,
+                      elevation: 2,
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            'View',
+                            style: GoogleFonts.abel(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white70),
+                          )),
+                    ),
+                    const Spacer(),
+                    const Icon(
+                      Icons.language,
+                      color: Colors.grey,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child: Text('Top Vendors'),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Manage Vendors',
+                      style: GoogleFonts.abel(
+                          fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  children: [
+                    MaterialButton(
+                      color: Colors.orange.shade200,
+                      elevation: 2,
+                      onPressed: () {},
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            'View',
+                            style: GoogleFonts.abel(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey),
+                          )),
+                    ),
+                    const Spacer(),
+                    const Icon(
+                      Icons.people,
+                      color: Colors.grey,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child: Text('Top Customers'),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Manage Customers',
+                      style: GoogleFonts.abel(
+                          fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
